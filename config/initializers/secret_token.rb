4ac517783f629645bbf10c8032c8f690b66b853f
Matthew Hannon <xspamx.zemeron@gmail.com>
2011-11-02 22:04:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd22d71612f6cf6a5e574c534dc45415a4a58d4ce1163f04b3f2e11709bc9b2a7b085119c3ef895594ec327fe2bfee0dcec6b34572504b23fe38adff772927450'

6d801ea8c4b561612ccd1239092aa5461e9d4564
Matthew Hannon <xspamx.zemeron@gmail.com>
2011-11-13 16:32:02 -0600

    added salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
 	
-	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
 	
 	validates :name, :presence => true,
 					 :length => { :maximum => 50}
@@ -14,6 +14,40 @@ class User < ActiveRecord::Base
 	validates :password, :presence => true,
 						:confirmation => true,
 						:length => { :within => 6..40}
+						
+	before_save :encrypt_password
+	
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	class << self
+		def authenticate(email, submitted_password)
+			user = find_by_email(email)
+			return nil if user.nil?
+			return user if user.has_password?(submitted_password)
+		end
+	end
+	
+	
+	private 
+		def encrypt_password
+			self.encrypted_password = encrypt(self.password)
+		end
+		
+		def encrypt(string)
+			self.salt = make_salt if self.new_record?
+			secure_hash("#{salt}--#{string}")
+		end
+		
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
+		
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+		
 end
 
 
